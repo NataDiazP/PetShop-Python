0042472fc58a83a8f90b5983df93a3ef4e1dfbb3
ti1Habla <ti1@hablacreativo.com>
2019-01-25 07:53:26 -0500

    Clase mensaje

---
+++ Mensajes.py
@@ -0,0 +1,34 @@
+class Mensajes:
+	mensajes_es = {
+		"welcome": "Bienvenido a Petshop",
+		"login": "1. Iniciar sesión",
+		"sign_up": "2. Registrarse",
+		"add_art": "3. Agregar artículo",
+		"see_art": "4. Ver articulos",
+		"add_data": "5. Agregar datos ficticios",
+		"see_art_price": "6. Ver precio de articulos con IVA",
+		"see_art_average_rate": "7. Ver artículo con mejor promedio de puntajes",
+		"edit_art_price": "8. Modificar precio de un artículo",
+		"exit": "9. Salir",
+		"menu_msg": "Seleccione la operación deseada: ",
+		"wrong_option": "Opción inválida, por favor intente nuevamente.",
+		"email": "Correo electrónico: ",
+		"password": "Contraseña: "
+	}
+
+	mensajes_eng = {
+		"welcome": "Welcome to Petshop",
+		"login": "1. Login",
+		"sign_up": "2. Sign up",
+		"add_art": "3. Add products",
+		"see_art": "4. See products",
+		"add_data": "5. Add dummie data",
+		"see_art_price": "6. See products price with taxes",
+		"see_art_average_rate": "7. See best rated product",
+		"edit_art_price": "8. Edit product price",
+		"exit": "9. Exit",
+		"menu_msg": "Select an option: ",
+		"wrong_option": "Wrong option, please try again.",
+		"email": "Email: ",
+		"password": "Password: "
+	}
