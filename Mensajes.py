5fb3ef57a3a6e7fc473fe719d4c892fe5b1f3287
ti1Habla <ti1@hablacreativo.com>
2019-01-25 22:02:11 -0500

    Nuevos mensajes

--- Mensajes.py
+++ Mensajes.py
@@ -1,33 +1,21 @@
 class Mensajes:
 	mensajes_es = {
-		"welcome": "Bienvenido a Petshop",
-		"login": "1. Iniciar sesión",
-		"sign_up": "2. Registrarse",
-		"add_art": "3. Agregar artículo",
-		"see_art": "4. Ver articulos",
-		"add_data": "5. Agregar datos ficticios",
-		"see_art_price": "6. Ver precio de articulos con IVA",
-		"see_art_average_rate": "7. Ver artículo con mejor promedio de puntajes",
-		"edit_art_price": "8. Modificar precio de un artículo",
-		"exit": "9. Salir",
-		"menu_msg": "Seleccione la operación deseada: ",
+		"welcome_menu": "Bienvenido a Petshop \nSeleccione la opción deseada: \n1. Generar datos ficticios \n2. Generar datos ficticios desde un txt \n3. Iniciar sesión \n4. Salir",
+		"user_type": "Seleccione el tipo de usuario: \n1. Cliente \n2. Empleado",
+		"client_login_menu": "1. Iniciar sesión \n2. Registrarse",
+		"client_menu": "1. Listar productos \n2. Seleccionar producto(s) \n3. Comentar producto \n4. Borrar comentario \n5. Ver carrito de compras \n6. Añadir producto a la lista de deseos \n7. Ver lista de deseos \n8. Listar pedidos realizados",
+		"employee_menu": "1. Crear empleados \n2. Desactivar empleados \n3. Eliminar empleados \n4. Crear productos \n5. Buscar productos \n6. Actualizar producto \n7. Eliminar producto \n8. Anular pedidos \n9. Ver pedidos del día"
 		"wrong_option": "Opción inválida, por favor intente nuevamente.",
 		"email": "Correo electrónico: ",
 		"password": "Contraseña: "
 	}
 
 	mensajes_eng = {
-		"welcome": "Welcome to Petshop",
-		"login": "1. Login",
-		"sign_up": "2. Sign up",
-		"add_art": "3. Add products",
-		"see_art": "4. See products",
-		"add_data": "5. Add dummie data",
-		"see_art_price": "6. See products price with taxes",
-		"see_art_average_rate": "7. See best rated product",
-		"edit_art_price": "8. Edit product price",
-		"exit": "9. Exit",
-		"menu_msg": "Select an option: ",
+		"welcome_menu": "Welcome to Petshop \nSelect the desired option: \n1. Generate dummy data \n2. Generate dummy data from a txt file \n3. Login \n4. Exit",
+		"user_type": "Select the user type: \n1. Client \n2. Employee",
+		"client_login_menu": "1. Login \n2. Sign up",
+		"client_menu": "1. List products \n2. Select product(s) \n3. Comment product \n4. Delete comment \n5. See shopping car \n6. Add product to whish list \n7. See wish list \n8. List previous orders",
+		"employee_menu": "1. Create employee \n2. Deactivate employee \n3. Delete employee \n4. Create product \n5. Search products \n6. Update product \n7. Delete product \n8. Cancel order \n9. See orders of the day"
 		"wrong_option": "Wrong option, please try again.",
 		"email": "Email: ",
 		"password": "Password: "
