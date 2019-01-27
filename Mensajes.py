5bbfb0e5267b84d5beef7c225b8d7e03f025f452
Mateo Martínez Palacio <43153303+MatMartinezPal@users.noreply.github.com>
2019-01-27 16:50:47 -0500

    Update Mensajes.py

--- Mensajes.py
+++ Mensajes.py
@@ -1,41 +1,46 @@
 class Mensajes:
+    mensajes_es = {
+        "welcome_menu": "\nBienvenido a Petshop \nSeleccione la opción deseada: \n\n1. Generar datos ficticios \n2. Generar datos ficticios desde un txt \n3. Iniciar sesión \n4. Salir",
+        "user_type": "\nSeleccione el tipo de usuario: \n\n1. Cliente \n2. Empleado \n3. Volver al menu principal",
+        "client_login_menu": "\nSelecciona una opcion\n\n1. Iniciar sesión \n2. Registrarse \n3. Volver al menu principal",
+        "succes_dummy_data": "\nLos datos han sido añadidos con exito",
+        "dummy_data_added": "\n----------------------------------------\nERROR\nYa agrego datos ficticios con anterioridad\n----------------------------------------",
+        "user_name": "\nNombre: ",
+        "user_email": "\nEmail: ",
+        "user_phone": "\nTelefono: ",
+        "user_address": "\nDireccion: ",
+        "user_password": "\nContraseña: ",
+        "error_login": "\n----------------------------------------\nERROR\nNo se pudo encontrar su direccion de correo electronico en el sistema\n----------------------------------------",
+        "error_register": "\n----------------------------------------\nERROR\nAlguien ya se ha registrado con ese email\n----------------------------------------",
+        "succes_login": "\n----------------------------------------\nHa iniciado sesion con exito\n----------------------------------------",
+        "succes_register": "\n----------------------------------------\nSe ha registrado con exito\n----------------------------------------",
+        "employee_login_menu": "\n1.Iniciar sesión\n2. Volver al menu principal",
+        "client_menu": ", Bienvenido PetShop, ¿Que desea hacer?\n\n1. Listar productos \n2. Seleccionar producto(s) \n3. Comentar producto \n4. Borrar comentario \n5. Ver carrito de compras \n6. Añadir producto a la lista de deseos \n7. Ver lista de deseos \n8. Listar pedidos realizados",
+        "employee_menu": "1. Crear empleados \n2. Desactivar empleados \n3. Eliminar empleados \n4. Crear productos \n5. Buscar productos \n6. Actualizar producto \n7. Eliminar producto \n8. Anular pedidos \n9. Ver pedidos del día",
+        "wrong_option": "\n----------------------------------------\nERROR\nOpción inválida. Por favor digite su seleccion nuevamente\n----------------------------------------",
+        "email": "Correo electrónico: ",
+        "password": "Contraseña: "
+    }
 
-	mensajes_es = {
-		"welcome_menu": "\nBienvenido a Petshop \nSeleccione la opción deseada: \n\n1. Generar datos ficticios \n2. Generar datos ficticios desde un txt \n3. Iniciar sesión \n4. Salir",
-		"user_type": "\nSeleccione el tipo de usuario: \n\n1. Cliente \n2. Empleado \n3. Volver al menu principal",
-		"client_login_menu": "\nSelecciona una opcion\n\n1. Iniciar sesión \n2. Registrarse \n3. Volver al menu principal",
-		"succes_dummy_data":"\nLos datos han sido añadidos con exito",
-		"dummy_data_added":"\n----------------------------------------\nERROR\nYa agrego datos ficticios con anterioridad\n----------------------------------------\n",
-		"user_name": "\nNombre: ",
-		"user_email": "\nEmail: ",
-		"user_phone": "\nTelefono: ",
-		"user_address": "\nDireccion: ",
-		"user_password": "\nContraseña: ",
-		"employee_login_menu": "\n1.Iniciar sesión\n2. Volver al menu principal",
-		"client_menu": ", Bienvenido PetShop, ¿Que desea hacer?\n\n1. Listar productos \n2. Seleccionar producto(s) \n3. Comentar producto \n4. Borrar comentario \n5. Ver carrito de compras \n6. Añadir producto a la lista de deseos \n7. Ver lista de deseos \n8. Listar pedidos realizados",
-		"employee_menu": "1. Crear empleados \n2. Desactivar empleados \n3. Eliminar empleados \n4. Crear productos \n5. Buscar productos \n6. Actualizar producto \n7. Eliminar producto \n8. Anular pedidos \n9. Ver pedidos del día",
-		"wrong_option": "Opción inválida, por favor intente nuevamente.",
-		"email": "Correo electrónico: ",
-		"password": "Contraseña: "
-	}
-
-	mensajes_eng = {
-		"welcome_menu": "\nWelcome to Petshop \nSelect the desired option: \n1. Generate dummy data \n2. Generate dummy data from a txt file \n3. Login \n4. Exit",
-		"user_type": "\nSelect the user type: \n\n1. Client \n2. Employee \n3. Go to the start menu",
-		"client_login_menu": "\nSelect the user option: \n\n1. Login \n2. Sign up \n3. Go back",
-		"succes_dummy_data": "\nData added successfully",
-		"dummy_data_added": "\n----------------------------------------\nERROR\nYou already add dummy data\n----------------------------------------\n",
-		"user_name": "\nName: ",
-		"user_email": "\nEmail: ",
-		"user_phone": "\nPhone number: ",
-		"user_address": "\nAddress: ",
-		"user_password": "\nPássword: ",
-		"employee_login_menu": "\n1.Login\n2. Go to the start menu",
-		"client_menu": ", Welcome to PetShop, What you want to do?\n\n1. List products \n2. Select product(s) \n3. Comment product \n4. Delete comment \n5. See shopping car \n6. Add product to whish list \n7. See wish list \n8. List previous orders",
-		"employee_menu": "1. Create employee \n2. Deactivate employee \n3. Delete employee \n4. Create product \n5. Search products \n6. Update product \n7. Delete product \n8. Cancel order \n9. See orders of the day",
-		"wrong_option": "Wrong option, please try again.",
-		"email": "Email: ",
-		"password": "Password: "
-	}
-
-
+    mensajes_eng = {
+        "welcome_menu": "\nWelcome to Petshop \nSelect the desired option: \n1. Generate dummy data \n2. Generate dummy data from a txt file \n3. Login \n4. Exit",
+        "user_type": "\nSelect the user type: \n\n1. Client \n2. Employee \n3. Go to the start menu",
+        "client_login_menu": "\nSelect the user option: \n\n1. Login \n2. Sign up \n3. Go back",
+        "succes_dummy_data": "\nData added successfully",
+        "dummy_data_added": "\n----------------------------------------\nERROR\nYou already added dummy data\n----------------------------------------",
+        "user_name": "\nName: ",
+        "user_email": "\nEmail: ",
+        "user_phone": "\nPhone number: ",
+        "user_address": "\nAddress: ",
+        "user_password": "\nPassword: ",
+        "error_login": "\n----------------------------------------\nERROR\nWe can't find your email in our database\n----------------------------------------",
+        "error_register": "\n----------------------------------------\nERROR\nSomeone already took your email\n----------------------------------------",
+        "succes_login": "\n----------------------------------------\nYou have logged in successfully\n----------------------------------------",
+        "succes_register": "\n----------------------------------------\nYou have registered successfully\n----------------------------------------",
+        "employee_login_menu": "\n1.Login\n2. Go to the start menu",
+        "client_menu": ", Welcome to PetShop, What do you want to do?\n\n1. List products \n2. Select product(s) \n3. Comment product \n4. Delete comment \n5. See shopping car \n6. Add product to whish list \n7. See wish list \n8. List previous orders",
+        "employee_menu": "1. Create employee \n2. Deactivate employee \n3. Delete employee \n4. Create product \n5. Search products \n6. Update product \n7. Delete product \n8. Cancel order \n9. See orders of the day",
+        "wrong_option": "\n----------------------------------------\nERROR\nWrong option. Please enter a valid option\n----------------------------------------",
+        "email": "Email: ",
+        "password": "Password: "
+    }
